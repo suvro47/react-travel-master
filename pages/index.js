238dbf3ec160346c9f4f0c972dfbe29f630ccd22
suvro47 <suvro047@gmail.com>
2021-08-24 16:20:46 +0600

    all-country-and-selected-country-dummy-data-added

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,19 @@ export default function Home() {
           <div className={styles.card}>
             <h2>Selected</h2>
             <ul>
-              
+              <li> Spain </li>
+              <li> Russia </li>
             </ul>
           </div>
 
           <div className={styles.card}>
             <h2>Choose From</h2>
             <ul>
-
+                <li> USA </li>
+                <li> Russia </li>
+                <li> Germany </li>
+                <li> Spain </li>
+                <li> Finland </li>  
             </ul>
           </div>
 
